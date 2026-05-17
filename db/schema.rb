7df3823f2f52467f8a9b9958131f91b1aa9718e9
ryeonthesulfur <perplexing2004@gmail.com>
2026-05-17 21:34:52 +0900

    データカラムの修正
    ・予約（reservations テーブル）と席（seats テーブル）のカラムの変更や追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_15_161911) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_17_120310) do
   create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.datetime "end_time", null: false
+    t.text "notes"
     t.integer "num_people", null: false
     t.string "phone_number", null: false
     t.bigint "seat_id", null: false
     t.datetime "start_time", null: false
-    t.string "status", null: false
-    t.integer "total_price", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["seat_id"], name: "index_reservations_on_seat_id"
@@ -30,9 +28,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_15_161911) do
     t.integer "capacity", null: false
     t.datetime "created_at", null: false
     t.integer "price_per_hour", null: false
-    t.integer "seat_number", null: false
+    t.string "seat_number", null: false
+    t.string "seat_type", null: false
+    t.bigint "store_id", null: false
     t.datetime "updated_at", null: false
-    t.index ["seat_number"], name: "index_seats_on_seat_number", unique: true
+    t.index ["store_id"], name: "index_seats_on_store_id"
+  end
+
+  create_table "stores", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "image_url"
+    t.string "name", null: false
+    t.boolean "smoking", default: false, null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -55,4 +63,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_15_161911) do
 
   add_foreign_key "reservations", "seats"
   add_foreign_key "reservations", "users"
+  add_foreign_key "seats", "stores"
 end
