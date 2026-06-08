b938d46a32a8a6ddd206827f212b9f2a147f839b
ryeonthesulfur <perplexing2004@gmail.com>
2026-06-08 17:25:02 +0900

    stores コントローラー編集
    ・予約済の座席データを取得する（@reserved_seat_numbers）
    
    Seat と Reservation モデルとのアソシエーションを利用して、「seat_id」（@store.seats）を介して、各メソッドによって、予約済の座席データを取得する。

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -9,8 +9,16 @@ class StoresController < ApplicationController
   def show
     # URLのidから店舗を取得
     @store = Store.find(params[:id])
+
     # 座席番号順に座席一覧を取得
     @seats = @store.seats.order(:seat_number)
+
+    # 予約中または使用中の座席番号を取得
+    @reserved_seat_numbers = @store.seats
+      .joins(:reservations)
+      .where(reservations: { status: [ :reserved, :using ] })
+      .pluck(:seat_number)
+
     # 座席タイプごとの席数を集計（店舗詳細画面のサマリー表示用）
     @seats_summary = @store.seats.group(:seat_type).count
   end
