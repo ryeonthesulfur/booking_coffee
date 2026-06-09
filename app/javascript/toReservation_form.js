6022c90f20a443e31fb9f4bfb7f32923bbf61eed
ryeonthesulfur <perplexing2004@gmail.com>
2026-06-09 14:03:38 +0900

    URLに予約日時を連結させる記述
    ・間取り図での座席選択画面にて、指定した予約日時を予約フォーム画面へのURLに連結させる記述

--- app/javascript/toReservation_form.js
+++ app/javascript/toReservation_form.js
@@ -4,9 +4,11 @@ function toReservationForm() {
 
     formButton.addEventListener('click', function() {
         const seatNumber = document.querySelector('input[name="seat_id"]:checked').value;
+        const startTime = document.getElementById('date-input').value; // 例: "2026-05-17 14:30〜"
         const storeId = formButton.getAttribute('data-store-id');
 
-        window.location.href = `/stores/${storeId}/seats/${seatNumber}/reservations/new`;
+        const url = `/stores/${storeId}/seats/${seatNumber}/reservations/new`;
+        window.location.href = startTime ? `${url}?start_time=${encodeURIComponent(startTime)}` : url;
 
 
 });
