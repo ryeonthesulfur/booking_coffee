304efc6be06fbe6bb964c1fd013e4825fe4f68df
ryeonthesulfur <perplexing2004@gmail.com>
2026-06-25 17:31:02 +0900

    メールリンクのホスト名変更
    ・EC2を再起動するたびに必要な処理

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "52.199.88.113" }
+  config.action_mailer.default_url_options = { host: "35.77.101.57" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
